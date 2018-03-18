1f51053f7a71da816f1bbefdf19bd08f874c2c6f
chrisbrake <nAp3ObtBG5yj>
2018-03-18 10:24:38 -0600

    Removed broken print statements.

--- ReST/ReST.py
+++ ReST/ReST.py
@@ -6,14 +6,12 @@ ephemeral_db = list()
 
 @route('/')
 def return_all():
-    print ephemeral_db
     return ephemeral_db
 
 
 @post('/')
 def append():
     ephemeral_db.append(json.load(request.body))
-    print ephemeral_db
 
 
 if __name__ == '__main__':
