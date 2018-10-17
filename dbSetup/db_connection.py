aa90fe2905f9a83f8927c23dab4cdede66e45c2c
chrisbrake <chris.brake@gmail.com>
2018-10-16 22:49:53 -0600

    Played with sqlalchemy a bit more

--- dbSetup/db_connection.py
+++ dbSetup/db_connection.py
@@ -1,6 +1,7 @@
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import IntegrityError
 
 Base = declarative_base()
 engine = sqlalchemy.create_engine('sqlite:///test.db', echo=True)
@@ -10,12 +11,12 @@ Session = sessionmaker(bind=engine)
 class User(Base):
     __tablename__ = 'users'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-    name = sqlalchemy.Column(sqlalchemy.String)
-    fullname = sqlalchemy.Column(sqlalchemy.String)
-    password = sqlalchemy.Column(sqlalchemy.String)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    fullname = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    password = sqlalchemy.Column(sqlalchemy.String, server_default='passwd')
 
     def __repr__(self):
-        return ("User(name='{s.name}', fullname='{s.fullname}', "
+        return ("User(id='{s.id}', name='{s.name}', fullname='{s.fullname}', "
                 "password='{s.password}')").format(s=self)
 
 
@@ -23,7 +24,15 @@ if __name__ == '__main__':
     Base.metadata.create_all(engine)
     ed_user = User(name='ed', fullname='Ed Jones', password='edspassword')
     session = Session()
-    session.add(ed_user)
-    session.commit()
-    our_user = session.query(User).filter_by(name='ed').all()
-    print(our_user)
+    # session.add(ed_user)  # This will throw an IntegrityError second run
+    session.merge(ed_user)  # This will not
+    try:
+        session.commit()
+    except IntegrityError as ie:
+        print('Error:', ie)
+        session.rollback()
+
+    our_users = session.query(User).filter_by(name='ed').all()
+    print('All users: {}'.format(our_users))
+    print('Filter example {i.id}: {i.fullname}'.format(
+        i=session.query(User).filter_by(id=1).one()))
