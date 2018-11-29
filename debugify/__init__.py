de81ef321ac8e13b66a0c2182708c6983d01af89
chrisbrake <chris.brake@gmail.com>
2018-11-28 22:30:36 -0700

    first pass at tox and pyproject.toml

--- debugify/__init__.py
+++ debugify/__init__.py
@@ -1,3 +1,11 @@
+""" I help with debugging """
+
 from debugify.DebugifyDecorator import debugify
 
 __all__ = [debugify]
+__version__ = "0.0.1"
+
+
+def hw():
+    """ Prints hello """
+    print('hello')
