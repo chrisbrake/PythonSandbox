7f7f2372903f8035f43548c2103af5f55c1af4ca
chrisbrake <chris.brake@gmail.com>
2019-03-08 22:25:19 -0700

    Comments

--- microWsgi/app.py
+++ microWsgi/app.py
@@ -10,31 +10,33 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 def status(req, resp):
+    """ Proof of life """
     return {"status": "ready"}
 
 
 def params(req, resp):
+    """ Params from the URL string """
     return req.params
 
 
 class Tester(object):
+    """ Sandbox """
 
     tests = {
         'db': db.diag,
-        'status': status,
         'params': params,
+        'status': status,
     }
 
     def on_get(self, req, resp, resource):
         if resource:
-            logging.debug('resource: %s', resource)
             resp.media = self.tests[resource](req, resp)
         else:
             resp.media = {k: v(req, resp) for k, v in self.tests.items()}
-            resp.media['test'] = 'potato'
-            logging.debug('data: %s', resp.data)
-            logging.debug('media: %s', resp.media)
+        logging.debug('data: %s', resp.data)
+        logging.debug('media: %s', resp.media)
         logging.debug('params: %s', req.params)
+        logging.debug('resource: %s', resource)
 
 
 def assemble():
