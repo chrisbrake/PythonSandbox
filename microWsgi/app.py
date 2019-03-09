0171d8cb20fb39f57a7cf701e5b0ea432bd711e7
chrisbrake <chris.brake@gmail.com>
2019-03-08 21:20:13 -0700

    free play

--- microWsgi/app.py
+++ microWsgi/app.py
@@ -1,3 +1,4 @@
+import logging
 import falcon
 
 """
@@ -5,22 +6,34 @@ uwsgi --module "microWsgi.app:assemble()" --http :5050
 
 curl -s http://localhost:5050 | python -m 'json.tool'
 """
+logging.basicConfig(level=logging.DEBUG)
 
 
 def ready(req, resp):
-    return {"ready": "yes"}
+    return {"status": "ready"}
+
+
+def params(req, resp):
+    return req.params
 
 
 class Tester(object):
 
     tests = {
         'ready': ready,
+        'params': params,
     }
 
     def on_get(self, req, resp, resource):
         if resource:
+            logging.debug('resource: %s', resource)
             resp.media = self.tests[resource](req, resp)
-        resp.media = {k: v(req, resp) for k, v in self.tests.items()}
+        else:
+            resp.media = {k: v(req, resp) for k, v in self.tests.items()}
+            resp.media['test'] = 'potato'
+            logging.debug('data: %s', resp.data)
+            logging.debug('media: %s', resp.media)
+        logging.debug('params: %s', req.params)
 
 
 def assemble():
