6de00ba16c6b594c65682fe2fcd8c678d7d72f5e
chrisbrake <chris.brake@gmail.com>
2020-02-11 22:10:09 -0700

    played with sqlite a bit more

--- dbSetup/sqlite.py
+++ dbSetup/sqlite.py
@@ -1,20 +1,47 @@
 import sqlite3
-conn = sqlite3.connect('example.db')
 
-c = conn.cursor()
+connection = sqlite3.connect('example.db')
+cursor = connection.cursor()
+
 # Create table
-c.execute('''CREATE TABLE IF NOT EXISTS stocks
-             (date text, trans text, symbol text, qty real, price real)''')
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS stocks
+    (date text, trans text, symbol text, qty real, price real)
+    ''')
 
 # Insert a row of data
-c.execute(
-    "INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+cursor.execute("""
+    INSERT INTO stocks 
+    VALUES ('2006-01-05','BUY','RHAT',100,35.14)
+    """)
 
 # Save (commit) the changes
-conn.commit()
+connection.commit()
 
-for row in c.execute('SELECT * FROM stocks ORDER BY price'):
+# Do this style value substitution
+t = ('RHAT',)
+cursor.execute('''
+    SELECT * 
+    FROM stocks 
+    WHERE symbol=?
+    ''', t)
+print(cursor.fetchone())
+
+# Larger example that inserts many records at a time
+cursor.executemany('''
+    INSERT INTO stocks 
+    VALUES (?,?,?,?,?)
+    ''', [
+    ('2006-03-28', 'BUY', 'IBM', 1000, 45.00),
+    ('2006-04-05', 'BUY', 'MSFT', 1000, 72.00),
+    ('2006-04-06', 'SELL', 'IBM', 500, 53.00),
+    ]
+)
+
+for row in cursor.execute('SELECT * FROM stocks ORDER BY price'):
     print(row)
 # We can also close the connection if we are done with it.
 # Just be sure any changes have been committed or they will be lost.
-conn.close()
+connection.close()
+
+# See also: https://docs.python.org/3.8/library/sqlite3.html#sqlite3.Connection.create_function
