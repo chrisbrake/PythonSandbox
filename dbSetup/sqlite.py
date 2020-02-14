fe1b478f93e31097cce62ead3266886bac1229fe
chrisbrake <chris.brake@gmail.com>
2020-02-13 21:57:40 -0700

    played with sqlite one last time today

--- dbSetup/sqlite.py
+++ dbSetup/sqlite.py
@@ -1,8 +1,11 @@
+import os
 import sqlite3
 import textwrap
 
-connection = sqlite3.connect("example.db")
-# connection = sqlite3.connect(":memory:")
+DB_PATH = os.environ.get('DB_PATH') or 'example.db'
+
+# connection = sqlite3.connect(DB_PATH)
+connection = sqlite3.connect(':memory:')
 cursor = connection.cursor()
 
 # Create table
@@ -11,29 +14,10 @@ cursor.execute('''
     (date text, trans text, symbol text, qty real, price real)
     ''')
 
-# Insert a row of data
-cursor.execute(
-    """
-    INSERT INTO stocks 
-    VALUES ('2006-01-05','BUY','RHAT',100,35.14)
-    """
-)
-
 # Save (commit) the changes
 connection.commit()
 
-# Do this style value substitution
-t = ('RHAT',)
-cursor.execute(
-    '''
-    SELECT * 
-    FROM stocks 
-    WHERE symbol=?
-    ''', t
-)
-print(cursor.fetchone())
-
-# Larger example that inserts many records at a time
+# print('We can easily insert many records at a time')
 cursor.executemany(
     '''
     INSERT INTO stocks 
@@ -42,13 +26,18 @@ cursor.executemany(
         ('2006-03-28', 'BUY', 'IBM', 1000, 45.00),
         ('2006-04-05', 'BUY', 'MSFT', 1000, 72.00),
         ('2006-04-06', 'SELL', 'IBM', 500, 53.00),
+        ('2006-01-05', 'BUY', 'RHAT', 100, 35.14),
     ]
 )
 
 print('You can iterate over the result in batches using cursor.fetchmany')
 cursor.execute('SELECT * FROM stocks ORDER BY price')
-for batch in cursor.fetchmany(2):
-    print(f'{[i for i in batch]}')
+batch = None
+i = 1
+while batch or batch is None:
+    batch = cursor.fetchmany(3)
+    print(f'\tBatch {i}: {batch}')
+    i = i + 1
 
 # Create a key-value data store
 cursor.execute('''
@@ -68,44 +57,35 @@ cursor.executemany(
 
 # Save (commit) the changes again
 connection.commit()
-print('And now we have a key/value store.')
-print([row for row in cursor.execute('SELECT * FROM kvs')])
-
+values = [row for row in cursor.execute("SELECT * FROM kvs")]
+print(textwrap.dedent(f'''
+    We can build a key/value store with mixed values: {values}
+'''))
 
 # We can also close the connection if we are done with it.
 # Just be sure any changes have been committed or they will be lost.
 connection.close()
 
-print('We can create functions and store them in the database.')
+print('We can store store functions in the connection to the database.')
 
 
 def sqlite_function(s):
-    return s
+    """
+    A method that will be passed into sqlite to be run as part of a query
+
+    :param s: argument
+    :return: a string
+    """
+    return f'sqlite_function({s})'
 
 
 con = sqlite3.connect('example.db')
 con.create_function('function', 1, sqlite_function)
 cur = con.cursor()
-cur.execute('select function(?)', (b"foo",))
+cur.execute('select function(?)', ('argument',))
 print(f'They can be executed as part of a select statement: {cur.fetchmany()}')
-
-cur.execute(
-    '''
-    select function('foo')
-    ''',
-)
-print(cur.fetchmany())
-
-cur.execute(
-    '''
-    select function('foo')
-    ''',
-)
-print(cur.fetchmany())
-
-
 con.close()
-con = sqlite3.connect('example.db')
+con = sqlite3.connect(DB_PATH)
 cur = con.cursor()
 
 try:
