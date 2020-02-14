d30e797beec28d2e9da56f890c316dc476d38e78
chrisbrake <chris.brake@gmail.com>
2020-02-13 21:17:26 -0700

    played with sqlite again today

--- dbSetup/sqlite.py
+++ dbSetup/sqlite.py
@@ -1,6 +1,8 @@
 import sqlite3
+import textwrap
 
-connection = sqlite3.connect('example.db')
+connection = sqlite3.connect("example.db")
+# connection = sqlite3.connect(":memory:")
 cursor = connection.cursor()
 
 # Create table
@@ -10,60 +12,112 @@ cursor.execute('''
     ''')
 
 # Insert a row of data
-cursor.execute("""
+cursor.execute(
+    """
     INSERT INTO stocks 
     VALUES ('2006-01-05','BUY','RHAT',100,35.14)
-    """)
+    """
+)
 
 # Save (commit) the changes
 connection.commit()
 
 # Do this style value substitution
 t = ('RHAT',)
-cursor.execute('''
+cursor.execute(
+    '''
     SELECT * 
     FROM stocks 
     WHERE symbol=?
-    ''', t)
+    ''', t
+)
 print(cursor.fetchone())
 
 # Larger example that inserts many records at a time
-cursor.executemany('''
+cursor.executemany(
+    '''
     INSERT INTO stocks 
     VALUES (?,?,?,?,?)
     ''', [
-    ('2006-03-28', 'BUY', 'IBM', 1000, 45.00),
-    ('2006-04-05', 'BUY', 'MSFT', 1000, 72.00),
-    ('2006-04-06', 'SELL', 'IBM', 500, 53.00),
+        ('2006-03-28', 'BUY', 'IBM', 1000, 45.00),
+        ('2006-04-05', 'BUY', 'MSFT', 1000, 72.00),
+        ('2006-04-06', 'SELL', 'IBM', 500, 53.00),
     ]
 )
 
-for row in cursor.execute('SELECT * FROM stocks ORDER BY price'):
-    print(row)
+print('You can iterate over the result in batches using cursor.fetchmany')
+cursor.execute('SELECT * FROM stocks ORDER BY price')
+for batch in cursor.fetchmany(2):
+    print(f'{[i for i in batch]}')
 
 # Create a key-value data store
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS kvs (key, value)
     ''')
 
-cursor.executemany('''
+cursor.executemany(
+    '''
     INSERT INTO kvs 
     VALUES (?,?)
     ''', [
-    ('2006-03-28', 45.00),
-    ('2006-04-05', 'BUY'),
-    ('2006-04-06', 500),
+        ("2006-03-28", 45.00),
+        ('2006-04-05', 'BUY'),
+        ('2006-04-06', 500),
     ]
 )
 
 # Save (commit) the changes again
 connection.commit()
+print('And now we have a key/value store.')
+print([row for row in cursor.execute('SELECT * FROM kvs')])
 
-for row in cursor.execute('SELECT * FROM kvs'):
-    print(row)
 
 # We can also close the connection if we are done with it.
 # Just be sure any changes have been committed or they will be lost.
 connection.close()
 
-# See also: https://docs.python.org/3.8/library/sqlite3.html#sqlite3.Connection.create_function
+print('We can create functions and store them in the database.')
+
+
+def sqlite_function(s):
+    return s
+
+
+con = sqlite3.connect('example.db')
+con.create_function('function', 1, sqlite_function)
+cur = con.cursor()
+cur.execute('select function(?)', (b"foo",))
+print(f'They can be executed as part of a select statement: {cur.fetchmany()}')
+
+cur.execute(
+    '''
+    select function('foo')
+    ''',
+)
+print(cur.fetchmany())
+
+cur.execute(
+    '''
+    select function('foo')
+    ''',
+)
+print(cur.fetchmany())
+
+
+con.close()
+con = sqlite3.connect('example.db')
+cur = con.cursor()
+
+try:
+    cur.execute(
+        '''
+        select function('foo')
+        ''',
+    )
+except sqlite3.OperationalError as e:
+    print(textwrap.dedent(f'''
+        But, they are a property of the connection and need to be explicitly 
+        included every time you reconnect connection the function is lost.
+        Otherwise you will see a "{e}" error.
+    '''))
+con.close()
